208c09ead396019301a96ad1b5cfad6f1a5fb635
ROG_BSCS <bscschatgpt@gmail.com>
2024-09-19 12:18:26 +0800

    Update hamburger

--- react-frontend/src/components/Navbar.js
+++ react-frontend/src/components/Navbar.js
@@ -16,7 +16,7 @@ function NavBar() {
             alt="Snapfolia"
           />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarOptions">
+        <Navbar.Toggle id="navbarBurger" aria-controls="navbarOptions">
           <span className="fas fa-bars color-green"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="navbarOptions" className="justify-content-center">
