fd1d5c5c8b1b3aa6b660b34dfac306b56447013a
Chinito <clark.wayne023@gmail.com>
2024-12-03 15:37:34 +0800

    Add Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/component/Navbar";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={`${inter.className} overflow-x-hidden`}>
         <Navbar />
         {children}
+        <Analytics />
       </body>
     </html>
   );
