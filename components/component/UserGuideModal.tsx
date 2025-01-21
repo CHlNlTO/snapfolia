314c2c48088b538337898c5116d283057ebe5d63
DevHansJTF <devhansjtf@gmail.com>
2025-01-21 13:46:04 +0800

    Uppercase locations and guide fix

--- components/component/UserGuideModal.tsx
+++ components/component/UserGuideModal.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
 import { useUserGuide } from "@/contexts/UserGuideContext";
 import UserGuide from "./UserGuide";
 
@@ -10,20 +9,10 @@ const UserGuideModal = () => {
   const { isOpen, setIsOpen } = useUserGuide();
 
   return (
-    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+    <Dialog open={isOpen} onOpenChange={() => {}} modal>
       <DialogContent className="sm:max-w-[600px] overflow-hidden md:mx-0 bg-transparent border-0 bg-none p-2 ring-0 focus:ring-0">
         <DialogTitle></DialogTitle>
-        <UserGuide />
-
-        <div className="flex justify-end relative">
-          <Button
-            variant="default"
-            onClick={() => setIsOpen(false)}
-            className="absolute top-0 -translate-x-4 -translate-y-[90px] mt-4 hover:ring-2 hover:ring-green-600 hover:ring-offset-2 hover:ring-offset-white bg-gradient-to-r from-green-500 to-green-600 border border-1 border-green-300 ring-1 ring-green-300"
-          >
-            Close
-          </Button>
-        </div>
+        <UserGuide onClose={() => setIsOpen(false)} isModal={true} />
       </DialogContent>
     </Dialog>
   );
