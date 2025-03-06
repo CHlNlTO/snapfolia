9560b370f178c68e26de017fdb33067056f023f5
Chinito <clark.wayne023@gmail.com>
2025-03-06 11:38:39 +0800

    Add PWA dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,49 @@
 /** @type {import('next').NextConfig} */
+import withPWA from "next-pwa";
+
 const nextConfig = {
   images: {
     domains: [
       "avatar.vercel.sh",
       "trees.firstasia.edu.ph",
       "uiffmgyykbhewphigjsf.supabase.co",
-    ], // Add the allowed domain here
+    ],
   },
 };
 
-export default nextConfig;
+const pwaConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  runtimeCaching: [
+    {
+      urlPattern:
+        /^https:\/\/uiffmgyykbhewphigjsf\.supabase\.co\/storage\/v1\/object\/public\/snapfolia\/.*/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "model-cache",
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+        },
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+      },
+    },
+    {
+      urlPattern: /^https:\/\/trees\.firstasia\.edu\.ph\/api\/.*/,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "api-cache",
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 60 * 60 * 24, // 24 hours
+        },
+      },
+    },
+  ],
+});
+
+export default pwaConfig(nextConfig);
