6bfacdf7620d37455fb1fc5d975533a81862e945
bscsgithub <bscschatgpt@gmail.com>
2025-04-20 16:46:51 +0800

    Update yml and 59 new species

--- flask-backend/configuration/config.py
+++ flask-backend/configuration/config.py
@@ -5,8 +5,8 @@ logger = logging.getLogger(__name__)
 
 class Config:
     UPLOAD_FOLDER = 'uploads'
-    YOLO_CLASSIFICATION_MODEL = os.path.join(os.path.dirname(__file__), 'model_files', 'classification.pt')
-    YOLO_OBJECT_DETECTION_MODEL = os.path.join(os.path.dirname(__file__), 'model_files', 'object_detection.pt')
+    YOLO_CLASSIFICATION_MODEL = os.path.join(os.path.dirname(__file__), 'model_files', '59_CLS.pt')
+    YOLO_OBJECT_DETECTION_MODEL = os.path.join(os.path.dirname(__file__), 'model_files', '58_OBJ.pt')
     
     if not os.path.exists(YOLO_CLASSIFICATION_MODEL):
         logging.debug(f"Error: YOLOv8 model file not found at {YOLO_CLASSIFICATION_MODEL}")
